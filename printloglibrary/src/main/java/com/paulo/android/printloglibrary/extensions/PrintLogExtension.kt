fceeae010ad92651b1991d05fe289a40688774ef
Paulo Eduardo Teixeira de Souza <pauloeduardo.teixeiradesouza-ext@ab-inbev.com>
2021-07-21 15:17:23 -0300

    v2.0.1 - Created helper and sample. Included process time

--- printloglibrary/src/main/java/com/paulo/android/printloglibrary/extensions/PrintLogExtension.kt
+++ printloglibrary/src/main/java/com/paulo/android/printloglibrary/extensions/PrintLogExtension.kt
@@ -2,29 +2,37 @@ package com.paulo.android.printloglibrary.extensions
 
 import android.annotation.SuppressLint
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.BALL
-import com.paulo.android.printloglibrary.constants.PrintLogConstants.BASE
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.BASE_1
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.BASE_3
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.BASE_4
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.BLANK_SPACE
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.BREAKLINE
-import com.paulo.android.printloglibrary.constants.PrintLogConstants.CLOSE_BRACKETS
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.CLOSE_KEYS
+import com.paulo.android.printloglibrary.constants.PrintLogConstants.CURSOR
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.DOUBLE_DOTS
+import com.paulo.android.printloglibrary.constants.PrintLogConstants.ELAPSED_TIME
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.FULL_DATA_FORMAT
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.HASHTAG
+import com.paulo.android.printloglibrary.constants.PrintLogConstants.HOURS
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.INDEX
+import com.paulo.android.printloglibrary.constants.PrintLogConstants.KEY
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.LINE
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.LOG
-import com.paulo.android.printloglibrary.constants.PrintLogConstants.OPEN_BRACKETS
+import com.paulo.android.printloglibrary.constants.PrintLogConstants.MINUTES
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.OPEN_KEYS
 import com.paulo.android.printloglibrary.constants.PrintLogConstants.POINTER
-import com.paulo.android.printloglibrary.constants.PrintLogConstants.SEMICOLON
-import com.paulo.android.printloglibrary.constants.PrintLogConstants.UNDER
-import java.lang.StringBuilder
+import com.paulo.android.printloglibrary.constants.PrintLogConstants.SECONDS
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
+
+fun getTitleFormatted(title: String, maxLength: Int): String {
+    val limit = 44 //Default
+    return if(title.length > limit) {
+        val substringMaxLength = maxLength - limit
+        title.substring(0, substringMaxLength)
+    } else title
+}
 
 fun getLine(): Int {
     return Throwable().stackTrace[4].lineNumber
@@ -52,10 +60,8 @@ fun buildLogText(time: String, line: Int, title: String, message: Any?): String 
     builder.append(DOUBLE_DOTS)
     builder.append(line)
     builder.append(CLOSE_KEYS)
-//    builder.append(BREAKLINE)
     builder.append(BLANK_SPACE)
     builder.append(BASE_1)
-//    builder.append(HASHTAG)
     builder.append(BLANK_SPACE)
     builder.append(title)
     builder.append(BLANK_SPACE)
@@ -96,25 +102,34 @@ fun buildNewBase(blockBaseContent: String, maxLength: Int): String {
 
 fun buildLists(value: Any?, builder: StringBuilder, i: Int): StringBuilder{
     var index = ""
-    index = when {
-        i < 10 -> "00$i"
-        i in 10..99 -> "0$i"
-        else -> i.toString()
+    index = when (i) {
+        in 0..9 -> "00$i"
+        in 10..99 -> "0$i"
+        in 100..999 -> "$i"
+        else -> ""
     }
     builder.append(HASHTAG)
     builder.append(BLANK_SPACE)
     builder.append(BLANK_SPACE)
     builder.append(OPEN_KEYS)
     builder.append(BALL)
-    builder.append(UNDER)
-    builder.append(INDEX)
-    builder.append(OPEN_KEYS)
-    builder.append(index)
-    builder.append(CLOSE_KEYS)
     builder.append(BLANK_SPACE)
-    builder.append(value)
+    if(index.isNotEmpty()) {
+        builder.append(INDEX)
+        builder.append(OPEN_KEYS)
+        builder.append(index)
+        builder.append(CLOSE_KEYS)
+        builder.append(CURSOR)
+        builder.append(BLANK_SPACE)
+        builder.append(value)
+    }
+    else {
+        builder.append(KEY)
+        builder.append(OPEN_KEYS)
+        builder.append(value)
+        builder.append(CLOSE_KEYS)
+    }
     builder.append(BLANK_SPACE)
-    builder.append(SEMICOLON)
     builder.append(BREAKLINE)
     return builder
 }
@@ -142,4 +157,27 @@ fun buildFooterStructureList(maxLength: Int, builder: StringBuilder): StringBuil
         builder.append(BASE_3)
     builder.append(BASE_1)
     return builder
+}
+
+fun buildTimeBlock(hours: String, mins: String, secs: String): String {
+    val builder = StringBuilder()
+    builder.append(ELAPSED_TIME)
+    builder.append(OPEN_KEYS)
+    builder.append(hours)
+    builder.append(HOURS)
+    builder.append(mins)
+    builder.append(MINUTES)
+    builder.append(secs)
+    builder.append(SECONDS)
+    builder.append(CLOSE_KEYS)
+    return builder.toString()
+}
+
+fun buildElapsedTime(blockBase: String, elapsedTime: String): String {
+    val builder = StringBuilder()
+    builder.append(blockBase)
+    builder.append(BASE_1)
+    builder.append(BLANK_SPACE)
+    builder.append(elapsedTime)
+    return builder.toString()
 }
